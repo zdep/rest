f15956b8b0728451c96951e9bd092d460f9c832c
dep <dep@mail.ru>
2021-02-02 20:03:32 +0300

    second commit

---
+++ testapi.sql
@@ -0,0 +1,14 @@
+﻿#
+# Table structure for table users
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Юзеры';
+INSERT INTO `users` VALUES (1,'test','111',222);
+INSERT INTO `users` VALUES (2,'test2','222',11);
